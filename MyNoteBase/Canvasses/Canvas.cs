b56458e42305749617374fa788d915f5a4d2468c
Schüler-BG12 <Schler-BG12@JPRS-PC0085>
2020-03-10 15:42:38 +0100

    Ein paar der Basisklassen hinzugefügt.

---
+++ MyNoteBase/Canvasses/Canvas.cs
@@ -0,0 +1,69 @@
+﻿using MyNoteBase.Classes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MyNoteBase.Canvasses
+{
+    [Serializable]
+    public class Canvas
+    {
+        private Image pixels => Manager.GetImage();
+        private IManager manager;
+        private DateTime dt;
+        private string name;
+        private Course course;
+
+        [XmlIgnore]
+        public IManager Manager
+        {
+            get
+            {
+                return manager;
+            }
+
+            set
+            {
+                manager = value;
+            }
+        }
+
+        public DateTime Dt
+        {
+            get
+            {
+                return dt;
+            }
+
+            set
+            {
+                dt = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Canvas(DateTime dt, string name, Course course, IManager manager)
+        {
+            this.dt = dt;
+            this.name = name;
+            this.course = course;
+            this.manager = manager;
+        }
+    }
+}
---
+++ MyNoteBase/Classes/Course.cs
@@ -0,0 +1,77 @@
+﻿using MyNoteBase.Canvasses;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNoteBase.Classes
+{
+    public class Course
+    {
+        private string name;
+        private Color color;
+        private List<Canvas> canvasses;
+        private Icon icon;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return color;
+            }
+
+            set
+            {
+                color = value;
+            }
+        }
+
+        public List<Canvas> Canvasses
+        {
+            get
+            {
+                return canvasses;
+            }
+
+            set
+            {
+                canvasses = value;
+            }
+        }
+
+        public Icon Icon
+        {
+            get
+            {
+                return icon;
+            }
+
+            set
+            {
+                icon = value;
+            }
+        }
+
+        public Course(string name, Color color, Icon icon)
+        {
+            this.name = name;
+            this.color = color;
+            this.icon = icon;
+        }
+    }
+}
